fae3d44cebe1ed212afea94d7d8e9d1e949c5a0d
shanu1981 <shanu.90158@gmail.com>
2024-01-17 16:11:13 +0530

    Added the logo of flipkart

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,12 +1,46 @@
 
+import { AppBar, Toolbar, styled, Typography, Box } from '@mui/material';
+import { yellow } from '@mui/material/colors';
 
 
+const StyledHeader = styled(AppBar)`
+    background : #2874f0;
+     height : 55px;
+`
+const Component = styled(Box)`
+    margin-left : 12%;
+    line-height : 0;
+`
+const SubHeading = styled(Typography)`
+    font-size : 10px;
+    font-style : italic;
+`
+const PlusImage = styled('img')`
+    width : 10px;
+    height : 10px;
+`
 
-function Header()
-{
-    return(
+function Header() {
+
+    const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
+    const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
+    return (
         <div>
-            This is sasank gadi ....
+            <StyledHeader>
+                <Toolbar>
+                <Component>
+
+                    <img src={logoURL} alt="logo" style={{ width: 75 }} />
+                    <Box style={{display : 'flex'}}> 
+                        <SubHeading> Explore 
+                            <Box component="span" style={{color : '#FFE500'}}> Plus
+                            <PlusImage src={subURL} alt="plus"/>
+                            </Box>
+                        </SubHeading>
+                    </Box>
+                </Component>
+                </Toolbar>
+            </StyledHeader>
         </div>
     )
 }
